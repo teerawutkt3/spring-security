f766b130d3542102bd4d62e4d153f7ec6371b9c4
teerawut(ท๊อปสุดหล่อ) <teerawutkt3@gmail.com>
2019-02-07 13:47:55 +0700

    add thymeleaf

--- src/main/java/com/project/cpeup/common/config/SecurityConfig.java
+++ src/main/java/com/project/cpeup/common/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.exceptionHandling().accessDeniedPage("/user/403")
 				.and()
 				.formLogin()
+				.loginPage("/login")
 				.permitAll()
 				.and()
 				.logout()
---
+++ src/main/resources/static/examSecu.sql
@@ -0,0 +1,82 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.35-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.5.0.5293
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for exam
+CREATE DATABASE IF NOT EXISTS `exam` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `exam`;
+
+-- Dumping structure for table exam.hibernate_sequence
+CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table exam.hibernate_sequence: 1 rows
+DELETE FROM `hibernate_sequence`;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+	(1);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+
+-- Dumping structure for table exam.role
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `description` varchar(150) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table exam.role: ~2 rows (approximately)
+DELETE FROM `role`;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` (`id`, `role_name`, `description`) VALUES
+	(1, 'ROLE_ADMIN', 'ROLE_ADMIN'),
+	(2, 'ROLE_USER', 'ROLE_USER');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+
+-- Dumping structure for table exam.role_assignment
+CREATE TABLE IF NOT EXISTS `role_assignment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) DEFAULT '0',
+  `user_id` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table exam.role_assignment: ~0 rows (approximately)
+DELETE FROM `role_assignment`;
+/*!40000 ALTER TABLE `role_assignment` DISABLE KEYS */;
+INSERT INTO `role_assignment` (`id`, `role_id`, `user_id`) VALUES
+	(1, 1, 1);
+/*!40000 ALTER TABLE `role_assignment` ENABLE KEYS */;
+
+-- Dumping structure for table exam.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `age` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table exam.user: ~0 rows (approximately)
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `username`, `password`, `name`, `last`, `age`) VALUES
+	(1, 'top', '$2a$10$gQ5o4nNvk8.eyLqbV4lRy.zI5J2CmtCQxaLIsmo7YDHlaaNZ4.qs2', 'top', 'top', 24);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
